f14a0425269623edbb0297a8465ece4144f6cf69
Bammidi Rajasekhar <bammidi.rajasekhar@gmail.com>
2014-01-28 20:39:32 +0530

    password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,21 @@ BookmarkApplication::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.raise_delivery_errors = true
+
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: 'gmail.com',
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: 'sekhar12345678@gmail.com',
+      password: 'sekhar!@#$%^&*'
+  }
+
+  config.action_mailer.default_url_options = { :host => 'sharebookmarks.herokuapp.com' }
+
 end
