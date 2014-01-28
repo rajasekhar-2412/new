07dbee79dbd2d4e88c2bdb17306599a63e4cea98
Bammidi Rajasekhar <bammidi.rajasekhar@gmail.com>
2014-01-28 15:22:13 +0530

    notice for updating username

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,9 @@ class SiteController < ApplicationController
   def index
     if current_user
       @bookmarks = current_user.bookmarks.order('created_at desc')
+        if current_user.username == ''
+          flash[:alert] = 'You dont have username. Please update your username'
+        end
     else
       @bookmarks = Bookmark.where(is_shared: true).order('created_at desc')
     end
