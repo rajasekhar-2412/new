6e00861f651942e81bb8bf26aebad661226c37c6
Bammidi Rajasekhar <bammidi.rajasekhar@gmail.com>
2014-01-29 10:53:47 +0530

    mail configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,9 @@ BookmarkApplication::Application.configure do
 
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.default_url_options = { :host => 'sharebookmarks.herokuapp.com' }
+
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
 
@@ -93,6 +96,6 @@ BookmarkApplication::Application.configure do
       password: 'sekhar!@#$%^&*'
   }
 
-  config.action_mailer.default_url_options = { :host => 'sharebookmarks.herokuapp.com' }
+
 
 end
